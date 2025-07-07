98550c39dc540447e66bb52cbaab663e3efe6fad
sahilbansal23 <bansal2210sahil@gmail.com>
2025-07-07 23:26:01 +0530

    adding project-details-api

--- controllers/masterData.js
+++ controllers/masterData.js
@@ -7,6 +7,7 @@ import dotenv from "dotenv";
 import {
   getLaneData,
   getPiuList,
+  getProjectDeatils,
   getProjectList,
   getRoList,
 } from "../queries/masterDataQueries.js";
@@ -110,6 +111,7 @@ const ProjectList = async (req, res) => {
     client.release();
   }
 };
+
 const laneData = async (req, res) => {
   const client = await pool.connect();
   const currentTimestamp = Date.now();
@@ -141,5 +143,36 @@ const laneData = async (req, res) => {
     client.release();
   }
 };
+const projectDeatils = async (req, res) => {
+  const client = await pool.connect();
+  const currentTimestamp = Date.now();
+  const content = req.body;
+  try {
+    logger.info("enter into project List");
+    logger.info(content);
+    const project_id = req.query.project_id;
+    await authvalidation(req.headers, client);
 
-export { roList, PiuList, ProjectList,laneData };
+    logger.info("user verified");
+
+    const getList = await client.query(getProjectDeatils, [project_id]);
+
+    res.status(200).send({
+      status: 200,
+      msg: "Data Returned Successfully",
+      data: getList.rows,
+    });
+  } catch (error) {
+    await client.query("ROLLBACK");
+    logger.error(error);
+    res.status(400).send({
+      status: 400,
+      msg: error.message,
+      data: req.body,
+    });
+  } finally {
+    client.release();
+  }
+};
+
+export { roList, PiuList, ProjectList,laneData ,projectDeatils};
