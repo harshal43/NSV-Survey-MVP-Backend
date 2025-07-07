a7b3a597629064b1511166f4c0c3d1f538efe53b
sahilbansal23 <bansal2210sahil@gmail.com>
2025-07-08 03:55:06 +0530

    correcting inspection by name

--- queries/inspectionQueries.js
+++ queries/inspectionQueries.js
@@ -1,7 +1,18 @@
 const projectInspection = `SELECT id, project_id,video_link, inspection_date, remarks, inspection_by, inspection_duration,created_at
 	FROM public.inspection WHERE project_id = $1 ORDER BY Created_at DESC`;
 
-const addInspectionQuery = `INSERT INTO public.inspection(id,
-	project_id, inspection_date, remarks, inspection_by, inspection_duration, video_link, created_at)
-	VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`;
+const addInspectionQuery = `SELECT 
+  i.id,
+  i.project_id,
+  i.inspection_date,
+  i.remarks,
+  u.name AS inspection_by,
+  i.inspection_duration,
+  i.video_link,
+  i.created_at
+FROM 
+  public.inspection i
+JOIN 
+  public."user" u ON i.inspection_by = u.id;
+`;
 export { projectInspection, addInspectionQuery };
